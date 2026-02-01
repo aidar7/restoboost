122cf42dc69f95ec5b2b491bdc3666d8ef9c2398
aidar7@gmail.com <aidar7@gmail.com>
2026-02-01 23:22:52 +0500

    fix: add frontend Render URL to CORS origins

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://restoboost-front.onrender.com",
         "http://127.0.0.1:3000",
         "http://localhost:8000",
         "http://127.0.0.1:8000",
